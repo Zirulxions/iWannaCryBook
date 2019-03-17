2ee5a35ee87f683445d01d50844cd1fabda94bfe
Javier Delgado <javier_180796@hotmail.com>
2019-03-17 13:27:08 -0400

    Tenth Commit
    Some Updates.

--- WebContent/Javascript/Logout.js
+++ WebContent/Javascript/Logout.js
@@ -29,11 +29,11 @@ function getAvatar(){
 			document.getElementById("userAvatar").src = data.url;
 			if(document.getElementById('profileAvatar')){
 				document.getElementById("profileAvatar").src = data.url;
-				if(document.getElementById('profile_Username') && document.getElementById('profile_Name')){
-					document.getElementById("profile_Username").innerText = data.username;
-					document.getElementById("profile_Name").innerText = data.name + " " + data.lastName;
-				}
 			}
+		}
+		if(document.getElementById('profile_Username') && document.getElementById('profile_Name')){
+			document.getElementById("profile_Username").innerText = data.username;
+			document.getElementById("profile_Name").innerText = data.name + " " + data.lastName;
 		}
 	})
 }
