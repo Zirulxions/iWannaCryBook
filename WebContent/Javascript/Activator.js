c1b0a9e8dacc3b168502c00367d9642cf45aca0f
Javier Delgado <javier_180796@hotmail.com>
2019-03-24 12:52:09 -0400

    Fixed Tabs
    JQuery sucks.

--- WebContent/Javascript/Activator.js
+++ WebContent/Javascript/Activator.js
@@ -1,3 +1,8 @@
-$(document).ready(function(){
-	$('.tabs').tabs();
-});
+var tabs = document.querySelectorAll('.tabs')
+for (var i = 0; i < tabs.length; i++){
+	M.Tabs.init(tabs[i]);
+}
+
+//$(document).ready(function(){
+//	$('.tabs').tabs();
+//});
