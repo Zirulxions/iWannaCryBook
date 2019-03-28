aa4a645055c31a475f75d6512680d846fe74e694
raulmalagarriga <raulmalagarriga@hotmail.com>
2019-03-28 13:28:04 -0400

    Add Friend
    backend, pojo.

---
+++ src/utility/FriendInnerClass.java
@@ -0,0 +1,14 @@
+package utility;
+
+public class FriendInnerClass {
+	private String username;
+	private Integer user_id;
+
+	public String getUsername() {
+		return username;
+	}
+	
+	public Integer getUserId() {
+		return user_id;
+	}	
+}
