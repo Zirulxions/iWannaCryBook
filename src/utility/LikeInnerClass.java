73b73146db4f08dab9e665f5d20c0ec1b5147389
raulmalagarriga <raulmalagarriga@hotmail.com>
2019-04-14 13:25:57 -0400

    likes backend
    POJO

---
+++ src/utility/LikeInnerClass.java
@@ -0,0 +1,32 @@
+package utility;
+
+public class LikeInnerClass {
+	private Integer userId;
+	private Integer postId;
+	private Integer typeLike;
+	
+	public Integer getUserId() {
+		return userId;
+	}
+	
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	public Integer getPostId() {
+		return postId;
+	}
+	
+	public void setPostId(Integer postId) {
+		this.postId = postId;
+	}
+	public Integer getTypeLike() {
+		return typeLike;
+	}
+	
+	public void setTypeLike(Integer typeLike) {
+		this.typeLike = typeLike;
+	}
+	
+	
+	
+}
