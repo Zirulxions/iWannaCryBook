a26a37a09d7c6c7d040a208f292123ba5f797739
raulmalagarriga <raulmalagarriga@hotmail.com>
2019-05-01 21:25:27 -0400

    Email
    class

---
+++ src/utility/Email.java
@@ -0,0 +1,55 @@
+package utility;
+
+import java.util.Properties;
+
+import javax.mail.Session;
+
+public class Email {
+	
+	Properties props = new Properties();
+
+	// host 
+	props.setProperty("mail.smtp.host", "smtp.gmail.com");
+
+	// TLS 
+	props.setProperty("mail.smtp.starttls.enable", "true");
+
+	// Puerto
+	props.setProperty("mail.smtp.port","587");
+
+	//  usuario
+	props.setProperty("mail.smtp.user", "ejemplo@gmail.com");
+
+	// Si requiere o no usuario y password para conectarse.
+	props.setProperty("mail.smtp.auth", "true");
+
+	Session session = Session.getDefaultInstance(props);
+
+	// Para obtener un log de salida más extenso
+	session.setDebug(true);
+	
+	
+	
+	
+	MimeMessage message = new MimeMessage(session);
+
+	// Quien envia 
+	message.setFrom(new InternetAddress("26053780r@gmail.com"));
+
+	// A quien va dirigido
+	message.addRecipient(Message.RecipientType.TO, new InternetAddress("destinatario@dominio.com"));
+
+	message.setSubject("iWannaCryBook");
+	message.setText("Your account have been banned");
+	
+	
+	
+	
+	Transport t = session.getTransport("smtp");
+
+	// Aqui usuario y password de gmail
+	t.connect("chuidiang@gmail.com","la password");
+	t.sendMessage(message,message.getAllRecipients());
+	t.close();
+}
+	
