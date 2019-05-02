5ebb075139262259db45e854537d122d906988aa
Javier Delgado <javier_180796@hotmail.com>
2019-05-02 01:33:03 -0400

    Search Bar. Emergency Commit

--- WebContent/Javascript/isAdminFunctionsScript.js
+++ WebContent/Javascript/isAdminFunctionsScript.js
@@ -4,7 +4,7 @@ function $(id) {
 
 var ulNav, liNav, aNav, iNav;
 var rowDiv, cols12Div, cardDiv, cardStackDiv, cardContentDiv, inputFieldDiv, inputFieldDiv2, cardActionDiv;
-var headH3, inputF1, label1, inputF2, label2, inputF3, label3, inputF4, label4, btnButton, ibtnButton;
+var headH3, inputF1, label1, inputF2, label2, inputF3, label3, inputF4, label4, btnButton, ibtnButton, iMatIcon;
 var optionHelp = 0;
 
 function alertingAdminMode(){
@@ -45,7 +45,6 @@ function alertingAdminMode(){
 		headH3.setAttribute("class","header");
 		if(count == 1){
 			headH3.innerHTML = "Ban or Unban an user.";
-			
 		} else if (count == 2){
 			headH3.innerHTML = "Update User Password";
 		}
@@ -61,7 +60,7 @@ function alertingAdminMode(){
 			inputFieldDiv.appendChild(inputF1);
 			label1 = document.createElement("label");
 			label1.setAttribute("for","idToBanUnban");
-			label1.setAttribute("class","");//maybe "";
+			label1.setAttribute("class","");
 			label1.innerHTML = "Ban / Unban User";
 			inputFieldDiv.appendChild(label1);
 		} else if(count = 2){
@@ -72,7 +71,7 @@ function alertingAdminMode(){
 			inputFieldDiv.appendChild(inputF1);
 			label1 = document.createElement("label");
 			label1.setAttribute("for","idUserPwChange");
-			label1.setAttribute("class","");//maybe "";
+			label1.setAttribute("class","");
 			label1.innerHTML = "User to change Password by Username";
 			inputFieldDiv.appendChild(label1);
 			inputF2 = document.createElement("input");
@@ -85,10 +84,9 @@ function alertingAdminMode(){
 			inputFieldDiv2.appendChild(inputF2);
 			label2 = document.createElement("label");
 			label2.setAttribute("for","pwChanged");
-			label2.setAttribute("class","");//maybe "";
+			label2.setAttribute("class","");
 			label2.innerHTML = "New Password here";
 			inputFieldDiv2.appendChild(label2);
-			
 		}
 		cardActionDiv = document.createElement("div");
 		cardActionDiv.setAttribute("class","card-action");
@@ -97,11 +95,11 @@ function alertingAdminMode(){
 		btnButton.setAttribute("class","btn waves-effect waves light");
 		btnButton.setAttribute("type","submit");
 		if(count == 1){
-			btnButton.setAttribute("onclick","optionHelp = 1; updateConfig();")
+			btnButton.setAttribute("onclick","optionHelp = 1; updateConfig();");
 			btnButton.setAttribute("id","updateBanStatus");
 			btnButton.innerHTML = "Ban/Unban User";
 		} else if(count == 2){
-			btnButton.setAttribute("onclick","optionHelp = 2; updateConfig();")
+			btnButton.setAttribute("onclick","optionHelp = 2; updateConfig();");
 			btnButton.setAttribute("id","updateUsPassword");
 			btnButton.innerHTML = "Change User's Password";
 		}
@@ -112,7 +110,59 @@ function alertingAdminMode(){
 		btnButton.appendChild(ibtnButton);
 	}
 	getAdminStats();
+	placeSearchBar();
 }
+
+
+function placeSearchBar(){
+	rowDiv = document.createElement("div");
+	rowDiv.setAttribute("class","row");
+	$("admFuncPanel").appendChild(rowDiv)
+	cols12Div = document.createElement("div");
+	cols12Div.setAttribute("class","col s12 m12");
+	rowDiv.appendChild(cols12Div);
+	cardDiv = document.createElement("div");
+	cardDiv.setAttribute("class","card horizontal");
+	cols12Div.appendChild(cardDiv);
+	cardStackDiv = document.createElement("div");
+	cardStackDiv.setAttribute("class","card-stacked");
+	cardDiv.appendChild(cardStackDiv);
+	cardContentDiv = document.createElement("div");
+	cardContentDiv.setAttribute("class","card-content");
+	cardStackDiv.appendChild(cardContentDiv);
+	headH3 = document.createElement("h3");
+	headH3.setAttribute("class","header");
+	headH3.innerHTML = "Search Things by Content";
+	cardContentDiv.appendChild(headH3);
+	inputFieldDiv = document.createElement("div");
+	inputFieldDiv.setAttribute("class","input-field col s12");
+	cardContentDiv.appendChild(inputFieldDiv);
+	inputF1 = document.createElement("input");
+	inputF1.setAttribute("id","searchInput");
+	inputF1.setAttribute("type","text");
+	inputF1.setAttribute("class","validate");
+	inputFieldDiv.appendChild(inputF1);
+	label1 = document.createElement("label");
+	label1.setAttribute("for","searchInput");
+	label1.setAttribute("class","");
+	label1.innerHTML = "Search Label";
+	inputFieldDiv.appendChild(label1);
+	cardActionDiv = document.createElement("div");
+	cardActionDiv.setAttribute("class","card-action");
+	cardStackDiv.appendChild(cardActionDiv);
+	btnButton = document.createElement("button");
+	btnButton.setAttribute("class","btn waves-effect waves light");
+	btnButton.setAttribute("type","submit");
+	btnButton.setAttribute("onclick","");
+	btnButton.setAttribute("id","searchNowButton");
+	btnButton.innerHTML = "Search Now";
+	cardActionDiv.appendChild(btnButton);
+	ibtnButton = document.createElement("i");
+	ibtnButton.setAttribute("class","material-icons right");
+	ibtnButton.innerHTML = "send";
+	btnButton.appendChild(ibtnButton);
+}
+
 
 function updateConfig(){
 	let body = {
@@ -203,6 +253,7 @@ function getAdminStats(){
 function showHideAdmPanel(){
 	if($("admFuncPanel").style.display == "none"){
 		$("admFuncPanel").style.display = "block";
+		//alert($("admFuncPanel").getAttribute("id"));
 	} else if ($("admFuncPanel").style.display == "block"){
 		$("admFuncPanel").style.display = "none";
 	}
