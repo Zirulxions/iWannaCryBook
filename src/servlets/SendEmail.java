a535eeee075309c043aca6dce7c249c19ed59685
raulmalagarriga <raulmalagarriga@hotmail.com>
2019-05-02 00:30:50 -0400

    Email, Serv
    servlet

--- src/servlets/SendEmail.java
+++ src/servlets/SendEmail.java
@@ -1,11 +1,27 @@
 package servlets;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.stream.Collectors;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import utility.DataBase;
+import utility.EmailInnerClass;
+import utility.PropertiesReader;
+import utility.Response;
 
 /**
  * Servlet implementation class SendEmail
@@ -13,6 +29,8 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/SendEmail")
 public class SendEmail extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private DataBase conn = new DataBase();
+	PropertiesReader prop = new PropertiesReader();
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -36,6 +54,29 @@ public class SendEmail extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+	}
+	private void email(Connection connection, HttpServletRequest request, HttpServletResponse response) throws JsonParseException, JsonMappingException, IOException {
+		ObjectMapper objMapper = new ObjectMapper();
+		PropertiesReader prop = PropertiesReader.getInstance();
+		EmailInnerClass emailInnerClass = objMapper.readValue(request.getReader().lines().collect(Collectors.joining(System.lineSeparator())), EmailInnerClass.class);
+		Response<EmailInnerClass> resp = new Response<>();
+		HttpSession session = request.getSession();
+		PreparedStatement stmt = null;
+		ResultSet result = null;
+		try {
+			System.out.println("Email");
+			stmt = connection.prepareStatement(prop.getValue("email"));
+			stmt.setString(1, emailInnerClass.getEmail());
+			result = stmt.executeQuery();
+		}catch(SQLException e){
+			System.out.println(e.getMessage());
+			resp.setData(null);
+			resp.setRedirect(null);
+			resp.setStatus(400);
+			resp.setMessage("Server Error.");
+			String res = objMapper.writeValueAsString(resp);
+			response.getWriter().print(res);
+		}
 	}
 
 }
